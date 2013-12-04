b440855d7239ebcf779487141d3d63234d581ac8
Eric Gauderman <gauderma@usc.edu>
2013-12-03 17:02:11 -0800

    Fixed compile errors in board.v

--- board.v
+++ board.v
@@ -2,31 +2,35 @@
 
 
 
-module Board(width, height, busWidth, // inputs
-	          reset, clk, // basic inputs
+module Board(reset, clk, // basic inputs
 			  readX, readY, // read inputs
 			  writeEn, writeX, writeY, writeValue, // write inputs
 	          readValue); // outputs
+	parameter width = 8, height = 8, busWidth = 8;
 	
-	input integer width, height, busWidth;
 	input reset, clk;
-	input readX[log2(width)-1 : 0], readY[log2(height)-1 : 0];
-	input writeEn, writeX[log2(width)-1 : 0], writeY[log2(height)-1 : 0], writeValue[busWidth-1 : 0];
-	output readValue[busWidth-1 : 0];
+	input [$clog2(width)-1 : 0] readX;
+	input [$clog2(height)-1 : 0] readY;
+	input writeEn;
+	input [$clog2(width)-1 : 0] writeX;
+	input [$clog2(height)-1 : 0] writeY;
+	input [busWidth-1 : 0] writeValue;
+	output [busWidth-1 : 0] readValue;
 	
-	reg [width*height-1 : 0] board[busWidth-1 : 0];
+	reg [busWidth-1 : 0] board[width*height-1 : 0];
 	
 	wire readI, writeI;
 	assign readI = readY*width + readX;
 	assign readValue = board[readI];
 	assign writeI = writeY*width + writeX;
 	
+	integer i = 0; // for the below for-loop
+	
 	always @(posedge reset)
 	begin : reset_block
-		integer i = 0;
 		for(i = 0; i < width*height; i = i+1)
 		begin
-			board[i] = busWidth'b0;
+			board[i] = 'b0;
 		end
 	end
 	
